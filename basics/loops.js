9de984162d9c9fcc3d6309e3496f67ebeb357139
willmclellarn <willmclellarn@gmail.com>
2021-02-12 14:58:05 -0800

    first js ls commit

---
+++ basics/loops.js
@@ -0,0 +1,46 @@
+// all looping assignments are here
+
+let i = 0;
+while (i <= 40) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+  i++;
+}
+
+// let cities = ['Istanbul', 'Los Angeles', 'Tokyo', null, 'Vienna', null, 'London', 'Beijing', null];
+// for (let i = 0; i < cities.length; i++) {
+//   if (cities[i] === null) {
+//     continue;
+//   } else
+//   console.log(cities[i].length);
+// }
+
+// let array = [1, 2, 3, 4];
+// let index = 0;
+//
+// while (index < array.length) {
+//   console.log(array[index]);
+//   index++;
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i * 2);
+// }
+
+// let greeting = 'Aloha!';
+// for (let i = 0; i < 3; i++) {
+//   console.log(greeting);
+// }
+
+//
+// for (let i = 10; i >= 1; i -= 1) {
+//   console.log(i);
+// }
+// console.log('Launch!')
+
+// for (let i = 0; i <= 10; i += 2) {
+//   // include your code here
+//   console.log(i);
+//   // it will log 0,2,4,6,8,10
+// }
